2053df1585055e806b4e0a4c89692c0cae2447bb
sirmcclay <mcclay.design@gmail.com>
2020-11-07 20:12:58 -0300

    Change endpoint to posts

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  // axios.post('http://localhost:4001/events', event);
+  // axios.post('http://localhost:4002/events', event);
+  // axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
 });
@@ -25,5 +25,6 @@ app.get('/events', (req, res) => {
 });
 
 app.listen(4005, () => {
+  console.log('Update to posts-clusterip');
   console.log('[Event-Bus] Listen on 4005');
 });
